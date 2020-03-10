8d56284867f6df684613a041e2d0d891610cadda
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 20:35:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT user.name, "
+  "SELECT user.name, COUNT(pledge.user_id) FROM user JOIN pledge ON user.id = pledge.user_id GROUP BY pledge.user_id ORDER BY SUM(pledge.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
