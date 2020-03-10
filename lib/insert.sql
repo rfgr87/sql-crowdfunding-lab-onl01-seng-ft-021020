48ca08276bfcf9c4c72e4ee73398bf18e186a398
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 18:57:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
-("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020")
+("Car Repair", "Businees", 2000.0, "01-01-2020", "02-20-2020"),
+("Field Trip", "Education", 500.0, "01-10-2019", "01-30-2019"),
+("Art Gallery", "Arts", 3000.0, "04-10-2020", "05-30-2020"),
+("Printing Shop", "Business", 10000.0, "04-20-2020", "04-22-2020"),
+("Music Album", "Arts", 5000.0, "03-05-2020", "03-30-2020"),
+("After School", "Education", 20000.0, "03-10-2020", "10-30-2020"),
+("Park", "Public", 100000.0, "05-10-2020", "12-30-2020"),
+("Music Group", "Arts", 6000.0, "01-10-2020", "02-30-2020"),
+("Shoe Repair Shop", "Business", 50000.0, "12-10-2020", "12-30-2020");
