7a3161ea6a2adc38dfc8187d6667072ca83ed7e3
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 18:52:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020"),
+("Science Fair", "Education", 1000.0, "03-10-2020", "03-30-2020")
