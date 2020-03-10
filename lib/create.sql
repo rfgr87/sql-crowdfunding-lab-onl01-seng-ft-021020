423dc41d241710fe5a23f3cd8c5bb4b781df29cd
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 21:25:48 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE projects (
 id INTEGER PRIMARY KEY,
-title TEXT,
+titles TEXT,
 category TEXT,
 funding_goal REAL,
 start_date TEXT,
@@ -8,7 +8,7 @@ end_date TEXT);
 
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
-name TEXT,
+names TEXT,
 age INTEGER);
 
 CREATE TABLE pledges (
