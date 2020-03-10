5e0e2f1e536cdc7c46c240e02103e7b23d224f91
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 21:39:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY pledges.project_id HAVING projects.funding_goal = SUM(pledgse.amounnt);"
+  "SELECT projects.title, SUM(pledges.amount) FROM pledges JOIN projects ON pledges.project_id = projects.id GROUP BY pledges.project_id HAVING projects.funding_goal = SUM(pledges.amounnt);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
